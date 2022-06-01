94e06accac5decaa6af527ccaafd1538bb537c81
HegedusAlexandra <hexapastry@gmail.com>
2022-06-01 11:26:19 +0200

    server

---
+++ backend/server2.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('../frontend'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/save',(req, res) => {
+    res.send(req.body)
+    res.send("")
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- frontend/script.js
+++ frontend/script.js
@@ -1,35 +1,38 @@
+// form js-sel,timeout,save
+
+    const googleSearchButton = document.querySelector("#btnGoogleSearch");
+
+    googleSearchButton.addEventListener("click",function (event) {
+        event.preventDefault();
+        console.log(event);
+
+        const form = document.querySelector("#googleSearch");
+
+        const searchTerm = form.querySelector("input[name='q']").value;
+
+        if (searchTerm === '') {
+            alert('Írj már be valamit!')
+        }else{
+            form.submit();
+            }
+            
+    })
+
+    setInterval(save,5000);
 
 
-const googleSearchButton = document.querySelector("#btnGoogleSearch");
+    async function save() {
+        const textarea = document.querySelector("#doc-text");
+        const docText = textarea.value;
 
-googleSearchButton.addEventListener("click",function (event) {
-    event.preventDefault();
-    console.log(event);
-
-    const form = document.querySelector("#googleSearch");
-
-    const searchTerm = form.querySelector("input[name='q']").value;
-
-    if (searchTerm === '') {
-        alert('Írj már be valamit!')
-    }else{
-        form.submit();
-        }
-        
-})
-
-setInterval(save,5000);
+        const saveText = document.querySelector("#save-in-progress-text");
+        saveText.classList.remove("hidden");
+        await fetch("http://127.0.0.1:3000/save", {
+            method: "POST",
+            body: docText
+        });
+        saveText.classList.add("hidden");
+    }
 
 
-async function save() {
-    const textarea = document.querySelector("#doc-text");
-    const docText = textarea.value;
-
-    const saveText = document.querySelector("#save-in-progress-text");
-    saveText.classList.remove("hidden");
-    await fetch("http://127.0.0.1:9001/", {
-        method: "POST",
-        body: docText
-    });
-    saveText.classList.add("hidden");
-}
+// form js-sel,timeout,save vége
